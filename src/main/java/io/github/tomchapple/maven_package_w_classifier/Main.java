ea1b735ea265aef76b61aa547cec3f81f794a440
Tom Chapple <tchapple@uow.edu.au>
2024-05-21 10:43:04 +1000

    Add basic project that builds multiple artifacts
    The goal here is to build a basic executable JAR, one with the sources
    and one with a custom, multi-word classifier.
    
    This is to test the theory that GitHub does not properly represent
    multi-word classifiers in its generated Maven Metadata file.

---
+++ src/main/java/io/github/tomchapple/maven_package_w_classifier/Main.java
@@ -0,0 +1,7 @@
+package io.github.tomchapple.maven_package_w_classifiers;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
