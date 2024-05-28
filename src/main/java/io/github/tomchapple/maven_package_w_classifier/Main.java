4e6dd70f8e394dd0ced5c9d0cf422c9a464ccbe5
Tom Chapple <tom.chapple@outlook.com>
2024-05-29 08:35:50 +1000

    Change message to "Goodbye, World!"

--- src/main/java/io/github/tomchapple/maven_package_w_classifier/Main.java
+++ src/main/java/io/github/tomchapple/maven_package_w_classifier/Main.java
@@ -2,6 +2,6 @@ package io.github.tomchapple.maven_package_w_classifiers;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Goodbye, World!");
     }
 }
